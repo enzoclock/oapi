1d3193988b5f51137c87d259f0740004f711c673
enzoclock <enzo.testa@oclock.io>
2024-12-18 13:15:22 +0100

    two more routes

--- api.js
+++ api.js
@@ -3,15 +3,27 @@ import { Router } from "express";
 import currenciesRates from "./data/currencies/rates.js";
 import currenciesInfo from "./data/currencies/details.js";
 import weatherCity from "./data/weather/cities.js";
+import blogPosts from "./data/blog/posts.js";
+import blogCategories from "./data/blog/categories.js";
 
 export const router = Router();
 
+// GET /api/currencies
 router.get("/currencies", (_, res) => { res.json(currenciesInfo); });
+
+// GET /api/currencies/rates
 router.get("/currencies/rates", (_, res) => { res.json(currenciesRates); });
 
+// GET /api/blog/posts
+router.get("/blog/posts", (_, res) => { res.json(blogPosts); });
+
+// GET /api/blog/categories
+router.get("/blog/categories", (_, res) => { res.json(blogCategories); });
+
+
+// GET /api/weather/cities(?random=true)
 router.get("/weather/cities", (req, res) => {
   const shouldRandomize = (req.query.random === "true"); 
-  
   if (shouldRandomize) {
     res.json(weatherCity.map(randomizeTemperature));
   } else {
---
+++ data/blog/categories.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    "id": 1,
+    "label": "Accueil",
+    "route": "/",
+  },
+  {
+    "id": 2,
+    "label": "Angular",
+    "route": "/angular",
+  },
+  {
+    "id": 3,
+    "label": "React",
+    "route": "/react",
+  },
+  {
+    "id": 4,
+    "label": "O’clock",
+    "route": "/oclock",
+  },
+  {
+    "id": 5,
+    "label": "Autre",
+    "route": "/autre",
+  }
+];
