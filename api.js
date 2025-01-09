48cf49399da9937188ca5c1e8ed5baa5b328877e
enzoclock <enzo.testa@oclock.io>
2025-01-09 16:55:48 +0100

    add route + 404 + refactor

--- api.js
+++ api.js
@@ -6,8 +6,10 @@ import weatherCity from "./data/weather/cities.js";
 import blogPosts from "./data/blog/posts.js";
 import blogCategories from "./data/blog/categories.js";
 import macarons from "./data/macarons/macarons.js";
+import recipes from "./data/recipes/recipes.js";
 
 export const router = Router();
+
 
 // GET /api/currencies
 router.get("/currencies", (_, res) => { res.json(currenciesInfo); });
@@ -25,7 +27,18 @@ router.get("/blog/categories", (_, res) => { res.json(blogCategories); });
 router.get("/macarons", (_, res) => { res.json(macarons); });
 
 // GET /api/weather/cities(?random=true)
-router.get("/weather/cities", (req, res) => {
+router.get("/weather/cities", weatherHandler);
+
+// GET /api/recipes
+router.get("/recipes", (_, res) => { res.json(recipes); });
+
+// Not found
+router.use((_, res) => { res.status(404).json({ status: 404, message: "Not found" }); })
+
+
+
+
+function weatherHandler(req, res) {
   const shouldRandomize = (req.query.random === "true"); 
   if (shouldRandomize) {
     res.json(weatherCity.map(randomizeTemperature));
@@ -38,5 +51,4 @@ router.get("/weather/cities", (req, res) => {
     const temperatureCelsius = city.temperatureCelsius + diff;
     return { ...city, temperatureCelsius };
   }
-});
-
+}
---
+++ data/recipes/recipes.js
@@ -0,0 +1,435 @@
+export default [
+  {
+    "id": 12345,
+    "title": "Cookies au beurre de cacahuète",
+    "slug": "cookies-beurre-de-cacahuete",
+    "thumbnail": "https://www.sunny-delices.fr/wp-content/uploads/2022/09/cookies-beurre-cacahouettes-sans-gluten-680x451.jpg",
+    "author": "Solene",
+    "difficulty": "Facile",
+    "description": "Pour 10 cookies",
+    "ingredients": [
+      {
+        "id": 1,
+        "quantity": 120,
+        "unit": "g",
+        "name": "sucre roux"
+      },
+      {
+        "id": 2,
+        "quantity": 120,
+        "unit": "g",
+        "name": "beurre de cacahuètes crunchy"
+      },
+      {
+        "id": 3,
+        "quantity": 120,
+        "unit": "g",
+        "name": "farine de riz"
+      },
+      {
+        "id": 4,
+        "quantity": 1,
+        "unit": "",
+        "name": "oeuf"
+      },
+      {
+        "id": 5,
+        "quantity": 0.5,
+        "unit": "cc",
+        "name": "bicarbonate"
+      },
+      {
+        "id": 6,
+        "quantity": 1,
+        "unit": "pincée",
+        "name": "sel"
+      },
+      {
+        "id": 7,
+        "quantity": 50,
+        "unit": "g",
+        "name": "pépites de chocolat"
+      }
+    ],
+    "instructions": [
+      "Mélangez au fouet l’oeuf, le sucre et le beurre de cacahuètes.",
+      "Ajoutez la farine, le bicarbonate et le sel, mélangez juste assez pour obtenir une pâte malléable.",
+      "Incorporez les pépites de chocolat.",
+      "Formez et déposez des boules de pâte sur une plaque recouverte de papier cuisson. ",
+      "Enfournez à 180°C pour 10 à 12 minutes de cuisson.",
+      "Laissez refroidir avant de les décoller de la plaque."
+    ]
+  },
+  {
+    "id": 8965,
+    "title": "Macaron framboisier",
+    "slug": "macaron-framboisier",
+    "thumbnail": "http://sunnybakery.fr/wp-content/uploads/2020/09/gateau-entremets-macaron-framboises.jpg",
+    "author": "Solène",
+    "difficulty": "Difficile",
+    "description": "Pour 8 personnes",
+    "ingredients": [
+      {
+        "id": 8,
+        "quantity": 2,
+        "unit": "",
+        "name": "blanc d'oeuf"
+      },
+      {
+        "id": 9,
+        "quantity": 100,
+        "unit": "g",
+        "name": "poudre d'amande"
+      },
+      {
+        "id": 10,
+        "quantity": 100,
+        "unit": "g",
+        "name": "sucre glaçe"
+      },
+      {
+        "id": 11,
+        "quantity": 100,
+        "unit": "g",
+        "name": "sucre en poudre"
+      },
+      {
+        "id": 12,
+        "quantity": 35,
+        "unit": "ml",
+        "name": "eau"
+      }
+    ],
+    "instructions": [
+      "Dans une petite casserole faites chauffer le sucre en poudre avec l’eau. Surveillez la température avec un thermomètre de cuisson.",
+      "En parallèle, montez un blanc d’œuf en neige pas trop ferme.",
+      "Dès que le sirop de sucre est à 118°C, versez le dans le blanc en neige, continuez de fouetter pendant encore 5min pour que la meringue refroidisse.",
+      "Mélangez l'autre blanc d'oeuf avec le sucre glace et la poudre d'amande",
+      "Incorporez délicatement et en plusieurs fois la meringue et 1 pincée de colorant rose en poudre.",
+      "Transvasez la pâte dans une poche à douille.",
+      "Sur une plaque à patisserie recouverte de papier cuisson, pochez 2 cercles de 20cm de diamètre.",
+      "Enfournez la plaque à 140°C pour 20 minutes de cuisson.",
+      "Garnissez les coques de framboises et de chantilly ou idéalement de crème au beurre à la meringue italienne."
+    ]
+  },
+  {
+    "id": 21453,
+    "title": "Tarte au citron meringuée",
+    "slug": "tarte-au-citron-meringuee",
+    "thumbnail": "https://www.sunny-delices.fr/wp-content/uploads/2020/04/tarte-au-citron-meringuee-sans-gluten-680x453.jpg",
+    "author": "Solène",
+    "difficulty": "Moyen",
+    "description": "Pour 8 à 10 personnes",
+    "ingredients": [
+      {
+        "id": 13,
+        "quantity": 200,
+        "unit": "g",
+        "name": "farine de riz complet"
+      },
+      {
+        "id": 14,
+        "quantity": 50,
+        "unit": "g",
+        "name": "poudre d'amande"
+      },
+      {
+        "id": 15,
+        "quantity": 100,
+        "unit": "g",
+        "name": "beurre"
+      },
+      {
+        "id": 16,
+        "quantity": 50,
+        "unit": "g",
+        "name": "sucre de canne"
+      },
+      {
+        "id": 17,
+        "quantity": 1,
+        "unit": "",
+        "name": "oeuf"
+      },
+      {
+        "id": 18,
+        "quantity": 4,
+        "unit": "",
+        "name": "citrons"
+      },
+      {
+        "id": 19,
+        "quantity": 100,
+        "unit": "g",
+        "name": "sucre"
+      },
+      {
+        "id": 20,
+        "quantity": 80,
+        "unit": "g",
+        "name": "beurre"
+      },
+      {
+        "id": 21,
+        "quantity": 2,
+        "unit": "",
+        "name": "oeufs entiers"
+      },
+      {
+        "id": 22,
+        "quantity": 2,
+        "unit": "",
+        "name": "jaunes d'oeufs"
+      },
+      {
+        "id": 23,
+        "quantity": 1,
+        "unit": "cs",
+        "name": "maïzéna"
+      },
+      {
+        "id": 24,
+        "quantity": 2,
+        "unit": "",
+        "name": "blancs d'oeufs pour la meringue"
+      },
+      {
+        "id": 25,
+        "quantity": 150,
+        "unit": "g",
+        "name": "sucre en poudre pour la meringue"
+      },
+      {
+        "id": 26,
+        "quantity": 60,
+        "unit": "ml",
+        "name": "eau pour la meringue"
+      }
+    ],
+    "instructions": [
+      "La pâte :",
+      "Mélangez la farine, la poudre d’amande, le sucre, le sel et le beurre coupé en petits morceaux.",
+      "Effritez le mélange entre les doigts jusqu’à obtenir une texture sableuse.",
+      "Ajoutez l’œuf et travaillez rapidement la pâte avec les mains jusqu’à formez une boule compacte et homogène.",
+      "Enveloppez la pâte dans du film alimentaire et laissez la reposer au frigo environ 1h.",
+      "Étalez la pâte puis déposez dans un moule à tarte.",
+      "Faîtes cuire 20 à 25 minutes à 180° (jusqu’à ce que la pâte colore légèrement).",
+      "La crème citron :",
+      "Mélangez dans une casserole la fécule de maïs, les œufs entiers, les jaunes, le jus et les zeste des citron et le sucre.",
+      "Faites chauffer à feu vif en mélangeant avec un fouet jusqu’à ce que le mélange épaississe.",
+      "Laissez la crème tiédir puis incorporez le beurre mou coupé en morceaux.",
+      "Nappez le fond de tarte avec la crème au citron.",
+      "La meringue italienne :",
+      "Dans une petite casserole faites chauffer le sucre en poudre avec l’eau. Surveillez la température avec un thermomètre de cuisson.",
+      "En parallèle, montez les blancs d’œuf en neige pas trop ferme.",
+      "Dès que le sirop de sucre est à 118°C, versez le dans les blancs en neige, continuez de fouetter pendant encore 5min pour que la meringue refroidisse.",
+      "Répartissez la meringue sur la tarte à la spatule ou à la poche à douille.",
+      "Décorez éventuellement avec quelques zestes de citron, des fleurs en sucre, des macarons…",
+      "Dégustez !"
+    ]
+  },
+  {
+    "id": 8762,
+    "title": "Amandier",
+    "slug": "amandier",
+    "thumbnail": "https://www.sunny-delices.fr/wp-content/uploads/2020/11/amandier-sans-gluten.jpg",
+    "author": "Solène",
+    "difficulty": "Facile",
+    "description": "Pour 6 personnes",
+    "ingredients": [
+      {
+        "id": 27,
+        "quantity": 100,
+        "unit": "g",
+        "name": "sucre"
+      },
+      {
+        "id": 28,
+        "quantity": 3,
+        "unit": "",
+        "name": "oeufs"
+      },
+      {
+        "id": 29,
+        "quantity": 20,
+        "unit": "g",
+        "name": "miel"
+      },
+      {
+        "id": 30,
+        "quantity": 150,
+        "unit": "g",
+        "name": "poudre d'amande"
+      },
+      {
+        "id": 31,
+        "quantity": 0.5,
+        "unit": "cc",
+        "name": "arome d'amande amère'"
+      },
+      {
+        "id": 32,
+        "quantity": 10,
+        "unit": "g",
+        "name": "arome fleur d'oranger"
+      },
+      {
+        "id": 33,
+        "quantity": "zeste",
+        "unit": "",
+        "name": "d'une orange"
+      },
+      {
+        "id": 34,
+        "quantity": 70,
+        "unit": "g",
+        "name": "beurre demi-sel"
+      },
+      {
+        "id": 35,
+        "quantity": 50,
+        "unit": "g",
+        "name": "mascarpone"
+      },
+      {
+        "id": 36,
+        "quantity": 40,
+        "unit": "g",
+        "name": "amandes effilées"
+      }
+    ],
+    "instructions": [
+      "Mélangez les oeufs avec le sucre et le miel. ",
+      "Ajoutez la poudre d’amande, le zeste d’orange, la fleur d’oranger et l’amande amère.",
+      "Ecrasez le beurre à la spatule jusqu’à ce qu’il ai une consistance crémeuse (beurre pommade).",
+      "Mélangez la mascarpone avec le beurre pommade et ajoutez à la préparation.",
+      "Mélangez bien la pâte au fouet à main puis versez dans un moule beurré.",
+      "Recouvrez la surface d’amandes effilées.",
+      "Enfournez à 150°C pour 45 minutes de cuisson."
+    ]
+  },
+  {
+    "id": 462,
+    "title": "Fondant au chocolat sans gluten",
+    "slug": "fondant-au-chocolat-sans-gluten",
+    "thumbnail": "https://www.sunny-delices.fr/wp-content/uploads/2015/06/fondant-chocolat-sans-gluten.jpg",
+    "author": "Solène",
+    "difficulty": "Facile",
+    "description": "Pour 8 personnes",
+    "ingredients": [
+      {
+        "id": 37,
+        "quantity": 200,
+        "unit": "g",
+        "name": "chocolat noir"
+      },
+      {
+        "id": 38,
+        "quantity": 125,
+        "unit": "g",
+        "name": "sucre"
+      },
+      {
+        "id": 39,
+        "quantity": 150,
+        "unit": "g",
+        "name": "beurre"
+      },
+      {
+        "id": 40,
+        "quantity": 70,
+        "unit": "g",
+        "name": "fécule de maïs"
+      },
+      {
+        "id": 41,
+        "quantity": 3,
+        "unit": "",
+        "name": "oeufs"
+      }
+    ],
+    "instructions": [
+      "Faites fondre le chocolat et le beurre ensemble",
+      "Battre les oeufs et le sucre",
+      "Incorporer le chocolat fondu",
+      "Ajouter la fécule de maïs en mélangeant bien",
+      "Versez dans un moule beurré.",
+      "Enfournez à 180°C pour 20 minutes de cuisson."
+    ]
+  },
+  {
+    "id": 11,
+    "title": "Tarte banoffee",
+    "slug": "tarte-banoffee",
+    "thumbnail": "https://www.sunny-delices.fr/wp-content/uploads/2020/04/banoffee-sans-gluten-1-680x453.jpg",
+    "author": "Soso",
+    "difficulty": "Facile",
+    "description": "Pour 6 personnes",
+    "ingredients": [
+      {
+        "id": 42,
+        "quantity": 1,
+        "unit": "",
+        "name": "pâte sablée"
+      },
+      {
+        "id": 43,
+        "quantity": 3,
+        "unit": "",
+        "name": "banane"
+      },
+      {
+        "id": 44,
+        "quantity": 160,
+        "unit": "g",
+        "name": "sucre en poudre"
+      },
+      {
+        "id": 45,
+        "quantity": 130,
+        "unit": "g",
+        "name": "crème liquide pour le caramel"
+      },
+      {
+        "id": 46,
+        "quantity": 120,
+        "unit": "g",
+        "name": "beurre demi-sel"
+      },
+      {
+        "id": 47,
+        "quantity": 300,
+        "unit": "g",
+        "name": "crème liquide pour la chantilly"
+      },
+      {
+        "id": 48,
+        "quantity": 150,
+        "unit": "",
+        "name": "mascarpone"
+      },
+      {
+        "id": 49,
+        "quantity": 60,
+        "unit": "",
+        "name": "sucre glace"
+      },
+      {
+        "id": 50,
+        "quantity": 1,
+        "unit": "gousse",
+        "name": "vanille"
+      }
+    ],
+    "instructions": [
+      "Pour le caramel, faites chauffer le sucre à feu moyen jusqu’à l’obtention d’un caramel",
+      "Ajoutez la crème chaude sur le caramel en mélangeant energiquement.",
+      "Incorporez le beurre coupé en morceau",
+      "Pour la chantilly, fouettez dans un récipient bien froid la crème, la mascarpone et le sucre glace.",
+      "Etalez la pate à tarte, foncez un moule et faites cuire à blanc 25 minutes à 170°C.",
+      "Couvrez le fond de tarte de tranches de banane.",
+      "Recouvrez de caramel.",
+      "Pochez la chantillly mascarpone."
+    ]
+  }
+]
